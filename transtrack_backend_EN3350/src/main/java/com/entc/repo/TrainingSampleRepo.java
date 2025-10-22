172064d9dc6a2e1308131080fb09894052c4c23c
sasindua07 <sasindu.jananga@gmail.com>
2025-10-22 20:01:32 +0530

    Changes_Feedback_Training

---
+++ transtrack_backend_EN3350/src/main/java/com/entc/repo/TrainingSampleRepo.java
@@ -0,0 +1,10 @@
+package com.entc.repo;
+
+import com.entc.model.TrainingSample;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface TrainingSampleRepo extends JpaRepository<TrainingSample, Long> {
+    long countBySentFalse();
+    List<TrainingSample> findTop50BySentFalseOrderByCreatedAtAsc();
+}
